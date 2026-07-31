116216dea4ad081791e81458ac9874ae4d2a168e
SnoochieBoochie <119842268+SnoochieBoochie@users.noreply.github.com>
2026-07-31 13:14:37 +0700

    Create sqr_triangle.cpp
    Напишите программу, которая проверяет, является ли треугольник прямоугольным.
    
    Формат ввода
    На стандартный поток ввода подаётся три целых положительных числа — стороны треугольника. Числа не превосходят 30000.
    
    Формат вывода
    Если полученный треугольник является прямоугольным, напечатайте YES. Если треугольник не является прямоугольным, напечатайте NO. Если с заданными сторонами невозможно построить треугольник, напечатайте UNDEFINED.

---
+++ sqr_triangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdint>  
+
+int main() {
+    int32_t a, b, c;
+    std::cin >> a >> b >> c;
+
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        std::cout << "UNDEFINED";
+    }
+    else {
+
+        if (a * a + b * b == c * c ||
+            a * a + c * c == b * b ||
+            b * b + c * c == a * a) {
+            std::cout << "YES";
+        }
+        else {
+            std::cout << "NO";
+        }
+    }
+
+    return 0;
+}
