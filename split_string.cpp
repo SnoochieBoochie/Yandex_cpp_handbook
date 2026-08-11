7c7ef5384972f19f021d0898dc1679ae8518d102
SnoochieBoochie <119842268+SnoochieBoochie@users.noreply.github.com>
2026-08-11 18:04:28 +0700

    Create split_string.cpp
    Вам требуется написать функцию со следующим заголовком:
    std::vector<std::string> Split(const std::string& str, char delimiter);
    Функция должна вернуть вектор строк, полученный разбиением строки str на части по указанному символу-разделителю delimiter. Если разделитель встретился в начале или в конце строки str, то в начале (соответственно, в конце) вектора с результатом должна быть пустая строка. Если два разделителя встретились рядом, то пустая строка между ними тоже должна попасть в ответ. Для пустой строки надо вернуть вектор, содержащий одну пустую строку.
    
    Например, Split("What_is_your_name?", '_') должна вернуть вектор из строк What, is, your и name?.

---
+++ split_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> Split(const std::string& str, char delimiter) {
+    std::vector<std::string> result;
+    std::string current;
+    
+    for (char c : str) {
+        if (c == delimiter) {
+            result.push_back(current);
+            current.clear();
+        } else {
+            current += c;
+        }
+    }
+    result.push_back(current);
+    
+    return result;
+}
