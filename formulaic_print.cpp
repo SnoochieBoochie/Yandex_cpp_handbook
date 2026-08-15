43fb24ce38d60ff297c24f7e628d106372f244dd
SnoochieBoochie <119842268+SnoochieBoochie@users.noreply.github.com>
2026-08-15 18:13:32 +0700

    Create formulaic_print.cpp
    Вам надо написать функцию Print, которая умеет печатать в поток std::cout элементы переданного контейнера через указанную строку-разделитель. Первый аргумент функции — контейнер. Гарантируется, что по этому контейнеру можно проитерироваться с помощью стандартного цикла range-based for, и что элементы контейнера можно напечатать в поток std::cout с помощью стандартного оператора <<. Второй аргумент функции — строка-разделитель, которую надо печатать между элементами. В конце необходимо напечатать перевод строки \n.
    
    Пример вызова:
    int main() {
        std::vector<int> data = {1, 2, 3, 4};
        Print(data, ", ");  // 1, 2, 3, 4
    }
    В вашем решении должен быть только код функции Print без функции main. Подключите все необходимые для реализации библиотеки. Используйте константные ссылки для получения параметров и при итерации в цикле, чтобы избежать лишних копирований: если этого не сделать, то программа не скомпилируется.

---
+++ formulaic_print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+template<typename Container>
+void Print(const Container& container, const std::string& delimiter) {
+    bool first = true;
+    for (const auto& element : container) {
+        if (!first) {
+            std::cout << delimiter;
+        }
+        std::cout << element;
+        first = false;
+    }
+    std::cout << '\n';
+}
